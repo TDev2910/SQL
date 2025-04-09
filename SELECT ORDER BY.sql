b081fb94fa72df2c4ccb894b5e91b7dd8f5d8979
Phạm Chí Trọng <122176490+TDev2910@users.noreply.github.com>
2025-04-09 16:33:46 +0700

    Add files via upload

---
+++ SELECT ORDER BY.sql
@@ -0,0 +1,30 @@
+﻿﻿-- Sắp xếp danh sách môn học theo độ dài tên môn học tăng dần.
+Select * From Subject
+Order by
+	LEN(Name) ASC
+
+﻿-- Sắp xếp danh sách môn học theo số tín chỉ giảm dần, tên tăng dần.
+Select * From Subject
+Order by 
+	Credit DESC,
+	Name ASC
+
+﻿-- Cho biết mã, tên môn học trong đó sắp xếp theo số tiết tăng dần.
+Select 
+	Id,
+	Name,
+	NumOfLesson
+From Subject
+Order by
+	NumOfLesson ASC
+
+﻿-- Cho biết mã, tên môn học, số tín chỉ của các môn học sắp xếp theo thứ tự từ điển(*) của loại môn học.
+Select 
+	Id,
+	Name,
+	Credit
+From Subject
+Order by
+	Type
+
+	
---
+++ SELECT TOP.sql
@@ -0,0 +1,29 @@
+﻿-- Cho biết top 10% sinh viên có ngày sinh nhỏ nhất.
+Select TOP 10 Percent With Ties 
+	Id,
+	FullName,
+	Day(BirthDate) as BirthDay
+From Student
+Order by
+	BirthDay ASC
+
+-- Cho biết top 5 sinh viên có tên dài nhất. Hiển thị cả các sinh viên tên cùng độ dài thỏa mãn.
+Select TOP 5 With Ties
+	Id,
+	FullName,
+	LEN(FullName) as Name
+From Student
+Order by
+	Name DESC
+
+﻿-- Cho biết top 3 môn học có số tiết học nhiều nhất.
+Select Top 3 With Ties *
+From Subject
+Order by
+	NumOfLesson DESC
+
+﻿-- Cho biết top 20% các môn học có số tín chỉ nhiều nhất.
+Select Top 20 Percent With Ties *
+From Subject
+Order by 
+	Credit DESC
